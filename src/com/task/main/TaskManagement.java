b12b3fbb689301b5e5a3dde21b853e1c1e0d6f18
prateeksri123 <me.prateek.srivastava@gmail.com>
2015-02-03 14:45:15 +0530

    Adding spring configuration

--- src/com/task/main/TaskManagement.java
+++ src/com/task/main/TaskManagement.java
@@ -1,7 +1,18 @@
 package com.task.main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.task.main.model.User;
+
 public class TaskManagement {
 	 public static void main(String[] args) {
 		    System.out.println("Hello Eclipse!");
+		    ApplicationContext context =
+	             new ClassPathXmlApplicationContext("Bean.xml");
+
+	      User obj = (User) context.getBean("helloWorld");
+	      System.out.println("Hello Eclipse! " + obj.getFirstName());
+	      obj.getFirstName();
 		  }
 }
