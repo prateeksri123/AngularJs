6f08c587a50e2ac93059b7217b1e06a7a81c988b
prateeksri123 <me.prateek.srivastava@gmail.com>
2015-02-03 15:12:05 +0530

    Adding the Spring communication with the hibernate

--- src/com/task/main/TaskManagement.java
+++ src/com/task/main/TaskManagement.java
@@ -1,18 +1,30 @@
 package com.task.main;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.task.main.model.User;
 
 public class TaskManagement {
-	 public static void main(String[] args) {
-		    System.out.println("Hello Eclipse!");
-		    ApplicationContext context =
-	             new ClassPathXmlApplicationContext("Bean.xml");
+	public static void main(String[] args) {
+		System.out.println("Hello Eclipse!");
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Bean.xml");
 
-	      User obj = (User) context.getBean("helloWorld");
-	      System.out.println("Hello Eclipse! " + obj.getFirstName());
-	      obj.getFirstName();
-		  }
+		User obj = (User) context.getBean("helloWorld");
+		System.out.println("Hello Eclipse! " + obj.getFirstName());
+		TestPerson tp = new TestPerson();
+		List<User> list = tp.getPersons();
+		java.util.Iterator<User> iter = list.iterator();
+		while (iter.hasNext()) {
+
+			User person = iter.next();
+			System.out.println("Person: \"" + person.getFirstName() + "\", "
+					+ person.getLastName() + "\", " + person.getId());
+
+		}
+
+	}
 }
